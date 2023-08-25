25e145eafc3d04039f666c166677797dd2d2feac
iskandar kodirov <eiscocyber@gmail.com>
2023-08-25 20:03:56 +0500

    update DTOs fixed

--- src/Edunaliz.Service/DTOs/Question/QuestionUpdateDto.cs
+++ src/Edunaliz.Service/DTOs/Question/QuestionUpdateDto.cs
@@ -8,7 +8,5 @@ public class QuestionUpdateDto
     public long Id { get; set; }
     public string Text { get; set; }
     public long? AttachmentId { get; set; }
-    public AttachmentResultDto Attachment { get; set; }
     public long AssignmentId { get; set; }
-    public AssignmentResultDto Assignment { get; set; }
 }
--- src/Edunaliz.Service/DTOs/Result/ResultUpdateDto.cs
+++ src/Edunaliz.Service/DTOs/Result/ResultUpdateDto.cs
@@ -7,9 +7,6 @@ namespace Edunaliz.Service.DTOs.Result;
 public class ResultUpdateDto
 {
     public long QuestionId { get; set; }
-    public QuestionResultDto Question { get; set; }
     public long UserId { get; set; }
-    public UserResultDto User { get; set; }
     public long AnswerId { get; set; }
-    public AnswerResultDto Answer { get; set; }
 }
